3d2a8cd709e3be5a44879b03c591185da089fae0
Jose Ruz <jose.ruz.dba@gmail.com>
2021-04-26 15:09:05 -0400

    trabajando ens earch

--- client/src/components/Card/Card.styles.js
+++ client/src/components/Card/Card.styles.js
@@ -3,11 +3,21 @@ const { default: styled } = require("styled-components");
 
 export const CardContainer = styled.div`
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 auto;
+    justify-content: center;
+    width: 200px;
+    height: 300px;
+
 
 `
 
 export const CardImage = styled.div`
 
+    img {
+        object-fit: cover;
+    }
 `
 
 export const CardInfo = styled.div`
--- client/src/components/Catalogue/Catalogue.styles.js
+++ client/src/components/Catalogue/Catalogue.styles.js
@@ -2,7 +2,10 @@ import styled from 'styled-components'
 
 
 export const CatalogueContainer = styled.div`
+    margin: 5rem auto; 
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(4, 1fr); //borrar esto en caso de neceitar infinite scroll
+    gap:25px;
+    max-width: 90%;
+
 `
