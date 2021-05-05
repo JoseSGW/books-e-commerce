4aeb0b184bc5888f56aee22792458071776f05e5
Jose Ruz <jose.ruz.dba@gmail.com>
2021-05-04 23:28:44 -0400

    Trabajando en slider precio

--- client/src/components/FilterBooks/FilterBooks.jsx
+++ client/src/components/FilterBooks/FilterBooks.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 import { FitlerContainer } from './FilterBooks.styles'
 
 
 export const FilterBooks = () => {
+
+    const [form, handleInputChange] = useForm({
+
+    })
+
     return (
         <FitlerContainer>
             <form action="">
-                
+                <label htmlFor="precio"></label>
+                <input type=""/>
             </form>
         </FitlerContainer>
     )
