9352bdaa1c275e5ac79b32092d64d674caef226d
Jose Ruz <jose.ruz.dba@gmail.com>
2021-05-06 18:40:39 -0400

    filtro mejorado diseño

--- client/src/components/FilterBooks/FilterBooks.jsx
+++ client/src/components/FilterBooks/FilterBooks.jsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useForm } from '../../hooks/useForm'
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 import { FitlerContainer } from './FilterBooks.styles'
 import { useFetch } from '../../hooks/useFetch';
 
@@ -30,35 +35,48 @@ export const FilterBooks = () => {
 
     return (
         <FitlerContainer>
-            <form action="">
-                <label htmlFor="precio">Precio</label>
-                <Range
-                    allowCross={false}
-                    defaultValue={filter.length > 0 ? [minAndMax[0].min_price, minAndMax[0].max_price] : [20, 800]}
-                    min={filter.length > 0 ? minAndMax[0].min_price : 0}
-                    max={filter.length > 0 ? minAndMax[0].max_price : 100}
-                />
+            <FormControl action="" className="form-container">
+                <fieldset>
 
-                <select name="authors" id="author">
-                    {
-                        authors?.map(author => <option value="volvo">{author.author}</option>)
-                    }
-                </select>
+                    <FormControl className="filter">
+                        <InputLabel id="demo-simple-select-label" htmlFor="author">Autor</InputLabel>
+                        <Select id="demo-simple-select-label" name="authors" id="author">
+                            {
+                                authors?.map(author => <MenuItem value="volvo">{author.author}</MenuItem>)
+                            }
+                        </Select>
+                    </FormControl>
 
-                <label htmlFor="years" type="text">Año</label>
-                <select name="years" id="years">
-                    {
-                        years?.map(year => <option value="volvo">{year.year}</option>)
-                    }
-                </select>
+                    <FormControl className="filter">
+                        <InputLabel htmlFor="years" type="text">Año</InputLabel>
+                        <Select name="years" id="years">
+                            {
+                                years?.map(year => <MenuItem value="volvo">{year.year}</MenuItem>)
+                            }
+                        </Select>
+                    </FormControl>
 
-                <label htmlFor="years" type="text">Generos</label>
-                <select name="years" id="years">
-                    {
-                        genres?.map(genre => <option value="volvo">{genre.name}</option>)
-                    }
-                </select>
-            </form>
+                    <FormControl className="filter">
+                        <InputLabel htmlFor="genres" type="text">Generos</InputLabel>
+                        <Select name="genres" id="genres">
+                            {
+                                genres?.map(genre => <MenuItem value="volvo">{genre.name}</MenuItem>)
+                            }
+                        </Select>
+                    </FormControl>
+
+                    <FormControl className="filter price-container">
+                        <InputLabel htmlFor="precio">Precio entre</InputLabel>
+                        <Range
+                            allowCross={false}
+                            defaultValue={filter.length > 0 ? [minAndMax[0].min_price, minAndMax[0].max_price] : [20, 800]}
+                            min={filter.length > 0 ? minAndMax[0].min_price : 0}
+                            max={filter.length > 0 ? minAndMax[0].max_price : 100}
+                        />
+                    </FormControl>
+                    <Button className="btn-filtrar" variant="outlined">Filtrar</Button>
+                </fieldset>
+            </FormControl>
         </FitlerContainer>
     )
 }
--- client/src/components/FilterBooks/FilterBooks.styles.js
+++ client/src/components/FilterBooks/FilterBooks.styles.js
@@ -4,7 +4,30 @@ export const FitlerContainer = styled.div`
     display: flex;
     flex-direction: column;
     margin: 0 auto;
-    height: 80vh;
+    max-height: 100%;
     width: 20vw;
     background-color: blanchedalmond;
-` 
+
+    .form-container{
+        display: flex;
+        flex-direction: column;
+        
+
+        fieldset{
+            .filter{
+                display: flex;
+                flex-direction: column;
+                margin: 2.5rem auto;
+                gap: .5rem;
+            }
+        }
+    }
+
+    .price-container{
+        margin-top: 3rem;
+    }
+
+    .btn-filtrar{
+        margin-top: 3rem;
+    }
+`
