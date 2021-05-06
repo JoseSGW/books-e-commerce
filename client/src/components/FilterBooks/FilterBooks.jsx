197f741e5ce7790b686381bf2655b79f81aef1d3
Jose Ruz <jose.ruz.dba@gmail.com>
2021-05-06 01:51:31 -0400

    los filtros ya reciben todo los datos

--- client/src/components/FilterBooks/FilterBooks.jsx
+++ client/src/components/FilterBooks/FilterBooks.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { useForm } from '../../hooks/useForm'
-import Slider, { Range } from 'rc-slider';
+import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import { FitlerContainer } from './FilterBooks.styles'
 import { useFetch } from '../../hooks/useFetch';
+
+
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
 
 
 export const FilterBooks = () => {
 
     const [filter, setFilter] = useState([])
 
-    /*  const [form, handleInputChange] = useForm({
- 
-     })
-  */
+    const [form, handleInputChange] = useForm({})
+
 
     useEffect(() => {
         fetch("http://localhost:3001/books/filteringOptions")
@@ -24,18 +26,38 @@ export const FilterBooks = () => {
     }, [])
 
 
-    const [author, minAndMax, years, genres] = !!filter && filter
-
+    const [authors, minAndMax, years, genres] = !!filter && filter
 
     return (
         <FitlerContainer>
             <form action="">
+                <label htmlFor="precio">Precio</label>
                 <Range
                     allowCross={false}
-                    defaultValue={filter.length > 0 ? [minAndMax[0].min_price, minAndMax[0].max_price] : [20, 80]}
+                    defaultValue={filter.length > 0 ? [minAndMax[0].min_price, minAndMax[0].max_price] : [20, 800]}
+                    min={filter.length > 0 ? minAndMax[0].min_price : 0}
+                    max={filter.length > 0 ? minAndMax[0].max_price : 100}
                 />
-                <label htmlFor="precio"></label>
-                <input type="" />
+
+                <select name="authors" id="author">
+                    {
+                        authors?.map(author => <option value="volvo">{author.author}</option>)
+                    }
+                </select>
+
+                <label htmlFor="years" type="text">Año</label>
+                <select name="years" id="years">
+                    {
+                        years?.map(year => <option value="volvo">{year.year}</option>)
+                    }
+                </select>
+
+                <label htmlFor="years" type="text">Generos</label>
+                <select name="years" id="years">
+                    {
+                        genres?.map(genre => <option value="volvo">{genre.name}</option>)
+                    }
+                </select>
             </form>
         </FitlerContainer>
     )
