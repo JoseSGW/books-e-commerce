9c071bac040be61c9be91576ac5a51716b000146
Jose Ruz <jose.ruz.dba@gmail.com>
2021-05-07 02:05:29 -0400

    arreglado filtro

--- client/src/components/FilterBooks/FilterBooks.jsx
+++ client/src/components/FilterBooks/FilterBooks.jsx
@@ -1,38 +1,48 @@
 import React, { useEffect, useState } from 'react'
 import { useForm } from '../../hooks/useForm'
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+//import Slider from 'rc-slider';
+//import 'rc-slider/assets/index.css';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
 import { FitlerContainer } from './FilterBooks.styles'
 import { useDispatch } from 'react-redux';
 import { setProductsByFilters, setProducts } from '../../actions/products';
 
 
-const createSliderWithTooltip = Slider.createSliderWithTooltip;
-const Range = createSliderWithTooltip(Slider.Range);
+/* const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range); */
 
 
 export const FilterBooks = () => {
 
     const [filter, setFilter] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [form, handleInputChange, setForm] = useForm({})
 
-    const dispatch =  useDispatch()
+    const dispatch = useDispatch()
 
     useEffect(() => {
         fetch("http://localhost:3001/books/filteringOptions")
             .then(data => data.json(data))
             .then(data => {
-                setFilter(data)
+                let { min_price, max_price } = data[1][0]
+                console.log(min_price)
+                setFilter([...data])
+                setForm({ ...form, range: [min_price, max_price] })
+                setLoading(true)
             })
     }, [])
 
     const [authors, minAndMax, years, genres] = !!filter && filter
 
-    const [form, handleInputChange, setForm] = useForm({})
+    let { min_price, max_price } = loading && minAndMax[0]
+
+
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -46,7 +56,7 @@ export const FilterBooks = () => {
 
                     <FormControl className="filter">
                         <InputLabel htmlFor="author">Autor</InputLabel>
-                        <Select onChange={handleInputChange} name="author" id="author">
+                        <Select defaultValue = "" onChange={handleInputChange} name="author" id="author">
                             {
                                 authors?.map(author => <MenuItem value={author.author}>{author.author}</MenuItem>)
                             }
@@ -55,7 +65,7 @@ export const FilterBooks = () => {
 
                     <FormControl className="filter">
                         <InputLabel htmlFor="year" type="text">Año</InputLabel>
-                        <Select onChange={handleInputChange} name="year" id="year">
+                        <Select defaultValue = "" onChange={handleInputChange} name="year" id="year" defaultValue="">
                             {
                                 years?.map(year => <MenuItem value={year.year}>{year.year}</MenuItem>)
                             }
@@ -64,7 +74,7 @@ export const FilterBooks = () => {
 
                     <FormControl className="filter">
                         <InputLabel htmlFor="genre" type="text">Generos</InputLabel>
-                        <Select onChange={handleInputChange} name="genre" id="genre">
+                        <Select defaultValue = "" onChange={handleInputChange} name="genre" id="genre">
                             {
                                 genres?.map(genre => <MenuItem value={genre.name}>{genre.name}</MenuItem>)
                             }
@@ -72,22 +82,37 @@ export const FilterBooks = () => {
                     </FormControl>
 
                     <FormControl className="filter price-container">
-                        <InputLabel htmlFor="precio">Precio entre</InputLabel>
-                        <Range
+
+                        <Typography id="range-slider" gutterBottom>
+                            Precio entre
+                        </Typography>
+                        <Slider
+                            name="range"
+                            value={loading && [form.range[0], form.range[1]]}
+                            onChange={(e, val) => handleInputChange({ target: { name: "range", value: val } })} 
+                            valueLabelDisplay="auto"
+                            aria-labelledby="range-slider"
+                            max={max_price}
+                            min={min_price}
+                        //getAriaValueText={valuetext}
+                        />
+                        {/* <InputLabel htmlFor="precio">Precio entre</InputLabel> */}
+
+                        {/* <Range
                             allowCross={false}
                             defaultValue={[20, 800]}
                             min={filter.length > 0 ? minAndMax[0].min_price : 0}
                             max={filter.length > 0 ? minAndMax[0].max_price : 100}
                             onAfterChange={(e) => handleInputChange({ target: { name: "range", value: e } })}
 
-                        />
+                        /> */}
                     </FormControl>
 
                     <FormControl className="botones-de-filtro">
                         <Button onClick={() => dispatch(setProducts())} className="btn-limpiar" variant="outlined">Limpiar filtro</Button>
                         <Button onClick={handleSubmit} className="btn-filtrar" variant="outlined">Filtrar</Button>
                     </FormControl>
-                    
+
                 </fieldset>
             </FormControl>
         </FitlerContainer>
