e9c057cf88234e00fd8df884aea76e6909c98ffe
Jose Ruz <jose.ruz.dba@gmail.com>
2021-05-21 21:56:03 -0400

    agregando funcionalidad a lista carrito

--- client/src/components/ProductsInCart/ProductsInCart.jsx
+++ client/src/components/ProductsInCart/ProductsInCart.jsx
@@ -3,7 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import CardMedia from '@material-ui/core/CardMedia';
-import { makeStyles } from '@material-ui/core';
+import { Button, Divider, makeStyles } from '@material-ui/core';
 
 const genericUrl = "https://www.julianmarquina.es/wp-content/uploads/Para-efecto-legales-un-libro-es-todo-impreso-no-periodico-que-contiene-49-paginas-o-mas.jpg"
 
@@ -24,18 +24,20 @@ export function ProductsInCart({ name, amount, id, images, price }) {
     const classes = useStyles();
 
     return (
-        <Container width='100%' className={classes.separate}>
-            <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '5rem', display: 'flex'}}>
-                <CardMedia className={classes.media} image={images[0] ? images[0].url : genericUrl} ></CardMedia>
-                <div className="name-product" style={{width: '60%'}}>
-                    <h2>{name}</h2>
-                </div>
-                <div className="price-product" style={{width: '20%'}}>
-                    <h2>{price}</h2>
-                </div>
-                <div className="detele-product" style={{width: '10%'}}>
-                    <h2>Eliminar</h2>
-                </div>
+        <Container className={classes.separate} style={{ backgroundColor: '#cfe8fc', height: '5rem', display: 'flex', width: '70%', padding: 0 }}>
+            <CardMedia className={classes.media} image={images[0] ? images[0].url : genericUrl} ></CardMedia>
+
+            <Typography component='h2' className="name-product" style={{ width: '50%', alignSelf: 'center', margin: '0 auto' }}>
+                {name}
+            </Typography>
+            <Typography component='h2' className="amount-product" style={{ width: '5%', alignSelf: 'center' }}>
+                {amount}
+            </Typography>
+            <Typography component='h2' className="price-product" style={{ width: '10%', alignSelf: 'center'}}>
+            $ {price * amount}
+            </Typography>
+            <Typography className="detele-product" style={{ width: '15%', alignSelf: 'center' }}>
+                <Button variant="contained" color="secondary">Eliminar</Button>
             </Typography>
         </Container>
     );
