1a7fa2df7ed1193e9e2f37e00688f212172721da
Jose Ruz <jose.ruz.dba@gmail.com>
2021-05-22 21:30:11 -0400

    arreglando componente mostrar total

--- client/src/components/ContainerProductsInCart/ContainerProductsInCart.jsx
+++ client/src/components/ContainerProductsInCart/ContainerProductsInCart.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import { useSelector } from 'react-redux';
 import { ProductsInCart } from '../ProductsInCart/ProductsInCart';
-import { Button, Typography } from '@material-ui/core';
-
+import { Box, Button, Typography } from '@material-ui/core';
+import { useTheme } from 'styled-components'
 
 
 
@@ -12,7 +12,12 @@ export function ContainerProductsInCart() {
 
     const { ShoppingCartProduct } = useSelector(state => state.shoppingCart)
 
+    const theme = useTheme()
+
+    let totalPrice = 0
+
     useEffect(() => {
+
     }, [ShoppingCartProduct]);
 
     return (
@@ -20,17 +25,27 @@ export function ContainerProductsInCart() {
             <Container style={{ maxWidth: '70%', display: 'flex', flexDirection: 'column', gap: '1.5rem' }}>
                 {
                     ShoppingCartProduct.map(p => {
-                        console.log(p);
+                        totalPrice += p.price * p.amount;
                         return <ProductsInCart {...p} />
-                    }
-                    )
+                    })
                 }
 
             </Container>
-            <Container style={{ backgroundColor: '#cfe8fc', height: '25rem', display: 'flex', flexDirection: 'column', maxWidth: '20%', gap: '2rem' }}>
-                <Typography>Total</Typography>
-                <Typography>Descuentos aplicados</Typography>
-                <Typography>Precio final a pagar</Typography>
+            <Container style={{ backgroundColor: theme.styles.colorPrimarioClaro, height: '25rem', display: 'flex', flexDirection: 'column', maxWidth: '20%', gap: '2rem' }}>
+                <Box bgcolor={theme.styles.colorSecundario} style={{padding: '1rem'}}>
+                    <Typography>Total</Typography>
+                    <Typography>{totalPrice}</Typography>
+                </Box>
+
+                <Box bgcolor={theme.styles.colorSecundario}>
+                    <Typography>Descuentos aplicados</Typography>
+                </Box>
+
+                <Box bgcolor={theme.styles.colorSecundario}>
+                    <Typography>Precio final a pagar</Typography>
+                    <Typography>{totalPrice}</Typography>
+                </Box>
+
                 <Button variant="contained" color="primary">Pagar</Button>
             </Container>
         </Container>
